9559f269bad45dd55223d3bb599dfbfb5aa320f7
Mathias Markl <mathias.markl@mukit.at>
2018-04-10 23:31:32 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -35,7 +35,7 @@ def read(fname):
 
 setup(
     name = "pdfconv",
-    version = "1.0.10",
+    version = "1.0.11",
     author = "Mathias Markl",
     author_email = "mathias.markl@mukit.at",
     description = "PDF Converter",
